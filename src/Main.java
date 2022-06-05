5acdd9a3beaed01e91b44c89ec643c0a3b7634e9
RachelLindquist <firefly7491@gmail.com>
2022-06-05 13:37:03 -0400

    SalesTax complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double SALES_TAX = 0.05;
+        double inputPrice = 25.00;
+        double computedSalesTax = inputPrice * SALES_TAX;
+        double totalPrice = inputPrice + computedSalesTax;
+
+        System.out.println("The input price was " + inputPrice + " and had a sales tax of " + computedSalesTax +
+                " for a total price of " + totalPrice);
     }
 }
